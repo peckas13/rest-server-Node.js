9f873ae8d836469583fd158a810c90f23cb66b45
Alex <alex13pks@gmail.com>
2019-11-11 09:25:12 -0600

    added connection string

--- server/config/config.js
+++ server/config/config.js
@@ -1,2 +1,16 @@
 //PUERTO
 process.env.PORT = process.env.PORT || 3000;
+
+//Entorno
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//Conexion a la base de datos
+let urlDB;
+
+if(process.env.NODE_ENV === 'dev'){
+    urlDB = 'mongodb://localhost:27017/cafeteria'
+}else{
+    urlDB = 'mongodb+srv://user:92duHqBFzwjJQYf@cluster0-fh2p2.mongodb.net/cafeteria';
+}
+
+process.env.URLDB = urlDB;
